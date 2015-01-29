1cc3efa4f591d9cf5ac680bea9fb8aa4ae043dc6
TS Lim <tslim@flexnode.com>
2015-01-29 15:27:47 +0800

    Switch to touchend instead

--- sites/showtimes.my/javascripts/application.js
+++ sites/showtimes.my/javascripts/application.js
@@ -11681,7 +11681,7 @@ Copyright (c) 2012-2013 Sasha Koss & Rico Sta. Cruz
 (function() {
   $(function() {
     return $("body").on({
-      'click touchstart': function(e) {
+      'click touchend': function(e) {
       	e.stopPropagation();
         if (window.innerWidth <= 768) {
           $(this).find(".movie-showtimes").toggle();
